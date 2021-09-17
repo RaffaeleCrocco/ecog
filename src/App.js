f899ba15efaa96c083e5901489eba5fb677c63ae
NicolaGianuzzi <61273196+NicolaGianuzzi@users.noreply.github.com>
2021-09-17 15:21:17 +0200

    Do App structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Dashboard from './pages/Dashboard';
+import Documentation from './pages/Documentation';
+import {Route, BrowserRouter as Router} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Route path="/" exact render={() => <Dashboard/>}/>
+        <Route path="/documentation" render={() => <Documentation/>}/>      
+      </Router>
     </div>
   );
 }
+
 
 export default App;
