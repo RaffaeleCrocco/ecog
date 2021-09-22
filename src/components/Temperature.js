1c2372f4feed5c047d3eb2045c7d6fbc3e4cfb4c
Raffaele Crocco <raffaelecrocco2000@gmail.com>
2021-09-22 23:47:24 +0200

    Fix temperature graph

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from "react";
 import { PieChart, Pie, Cell } from 'recharts';
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Popover, OverlayTrigger } from "react-bootstrap";
 import database from '../data/data.json'
 import fakeSensor from '../data/fake-sensors-data.json'
 import {AiOutlineFire} from "react-icons/ai" 
@@ -22,6 +22,16 @@ const Temperature = (props) => {
     var scarto = (current < ideal) ? ideal - current : current - ideal
 
 
+    const popover = (
+        <Popover id="popover-basic">
+            <Popover.Header as="h3">Graph purpose</Popover.Header>
+            <Popover.Body>
+                The purpose of the graph is to show the waste between the current and the ideal situation:
+                the system is <strong>{scarto}°C</strong> distant form the best temperature.
+            </Popover.Body>
+        </Popover>
+    );
+
     
     const data = [
         { name: "Group A", value: current },
@@ -33,24 +43,26 @@ const Temperature = (props) => {
     return (
         <Container>
             <Row>
-                <Col className="d-flex justify-content-end align-items-center" xs="auto">
-                    <PieChart width={180} height={180} onMouseEnter={onPieEnter}>
-                        <Pie
-                            data={data}
-                            cx={90}
-                            cy={90}
-                            innerRadius={60}
-                            outerRadius={80}
-                            fill="#8884d8"
-                            paddingAngle={5}
-                            dataKey="value"
-                        >
-                            {
-                            data.map((entry, index) => (<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />))
-                            }
-                        </Pie>
-                    </PieChart>
-                </Col>
+                <OverlayTrigger placement="right" overlay={popover}>
+                    <Col className="d-flex justify-content-end align-items-center" xs="auto">
+                        <PieChart width={180} height={180} onMouseEnter={onPieEnter}>
+                            <Pie
+                                data={data}
+                                cx={90}
+                                cy={90}
+                                innerRadius={60}
+                                outerRadius={80}
+                                fill="#8884d8"
+                                paddingAngle={0}
+                                dataKey="value"
+                            >
+                                {
+                                data.map((entry, index) => (<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />))
+                                }
+                            </Pie>
+                        </PieChart>
+                    </Col>
+                </OverlayTrigger>
                 <Col className="d-flex justify-content-center align-items-center">
                     <div>
                         <h4>Temperature</h4>
@@ -60,7 +72,7 @@ const Temperature = (props) => {
                             and {database[props.plant].stats.temperature[1]}°C. Indeed the best temperature is <strong>{database[props.plant].stats.temperature[2]}°C</strong>.
                         </p>
                         <ul>
-                           <li>Current temperature: <strong>{current}°</strong></li>
+                            <li>Current temperature: <strong>{current}°</strong>.</li>
                         </ul>
                     </div>
                 </Col> 
