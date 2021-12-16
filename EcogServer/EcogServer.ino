aa14813e7a6bd1c552f385a3d5e91526b75da5d2
Raffaele Crocco <raffaelecrocco2000@gmail.com>
2021-12-16 15:00:38 +0100

    Add more comments

--- EcogServer/EcogServer.ino
+++ EcogServer/EcogServer.ino
@@ -1,3 +1,7 @@
+//##################################
+//            includes
+//##################################
+
 #include <ESP8266WiFi.h>
 #include "aWOT.h"
 #include "StaticFiles.h"
@@ -6,24 +10,30 @@
 #include "DS18B20.h" //temp sensor for terrain
 #include "photoresistor.h" //light sensor
 
+//##################################
+//     intial configuration
+//##################################
 
-
+//change this with your home wifi credential
 #define WIFI_SSID "TIM-58389203"
 #define WIFI_PASSWORD "c36bkPbKh7SCT6zAd3HH3yQ7"
+
+//define pin for each sensor
 #define LED 5
 #define DHT11_PIN 4
 #define MOIST_PIN 0
 #define DS18B20_PIN 14
 #define PHOTORESISTOR_PIN 12
 
-//define object from costructors
+//define object using costructors
 DHTesp DHT;
 moist MOIST;
 ds18b20 DS18B20;
 photoresistor PHOTORESISTOR;
-
 WiFiServer server(80);
 Application app;
+
+//define variables to store sensors data
 bool ledOn;
 float airTemp;
 float airHum;
@@ -31,6 +41,9 @@ float terrHum;
 float terrTemp;
 bool light;
 
+//##################################
+//     functions to send data
+//##################################
 
 void readAirTemp(Request &req, Response &res){
   airTemp = DHT.getTemperature();
@@ -67,15 +80,18 @@ void updateLed(Request &req, Response &res){
   return readLed(req, res);
 }
 
+//##################################
+//         setup function
+//##################################
+
 void setup() {
-  pinMode(LED, OUTPUT);
-  DHT.setup(DHT11_PIN, DHTesp::DHT11);
-  //pinMode(DHT11_PIN, INPUT); already in the DHT11 library
 
+  pinMode(LED, OUTPUT); //setting led pin as an output
+  DHT.setup(DHT11_PIN, DHTesp::DHT11); //setting up the dht sensor
   
-  
-  Serial.begin(115200);
+  Serial.begin(115200); //setting the serial to print IP the device reserve for itself
 
+  //wifi configuration and IP printing
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -83,31 +99,23 @@ void setup() {
   }
   Serial.println(WiFi.localIP());
 
-  //method for internal light
+  //method for sensors
   app.get("/led", &readLed);
   app.put("/led", &updateLed);
-
-  //get for dht11
   app.get("/airtemp", &readAirTemp);
   app.get("/airhum", &readAirHum);
-  
-  //get for fork
   app.get("/terrhum", &readTerrHum);
-  
-  //get for probe
   app.get("/terrtemp", &readTerrTemp);
-
-  //get for light sensor
   app.get("/light", &readLight);
 
   app.use(staticFiles());
-
-  server.begin();
+  server.begin(); //starting the server
 }
 
 void loop() {
   WiFiClient client = server.available();
 
+  //we keep processing data until the client is connected to the server
   if (client.connected()) {
     app.process(&client);
   }
