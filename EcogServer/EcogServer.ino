d0c4957192b3c25cd3135ec44f7f5aaea213f452
Raffaele Crocco <raffaelecrocco2000@gmail.com>
2022-02-02 21:45:04 +0100

    Add display management

--- EcogServer/EcogServer.ino
+++ EcogServer/EcogServer.ino
@@ -10,6 +10,10 @@
 #include "photoresistor.h"     //light sensor
 #include "temperatureSensor.h" //temp sensor for terrain
 
+#include <Wire.h>              // Display
+#include <Adafruit_GFX.h>      //
+#include <Adafruit_SSD1306.h>  //
+
 //##################################
 //     intial configuration
 //##################################
@@ -24,6 +28,10 @@
 #define MOIST_PIN 0
 #define DS18B20_PIN 14
 #define PHOTORESISTOR_PIN 12
+
+// define display constants
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 // define object using costructors
 DHTesp DHT;
@@ -40,6 +48,9 @@ float airHum;
 float terrHum;
 float terrTemp;
 bool light;
+
+// define display object
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 //##################################
 //     functions to send data
@@ -99,15 +110,34 @@ void setup()
   DHT.setup(DHT11_PIN, DHTesp::DHT11); // setting up the dht sensor
 
   Serial.begin(115200); // setting the serial to print IP the device reserve for itself
-
+  
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // check display allocation
+    Serial.println(F("SSD1306 allocation failed"));
+    while (true);
+  }
+  display.display();
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.cp437(true);
+  
   // wifi configuration and IP printing
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
+  
+  while (WiFi.status() != WL_CONNECTED){
+    display.setCursor(0, 0);
+    display.print("Connecting");
+    display.display();
   }
-  Serial.println(WiFi.localIP());
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("Connected");
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 20);
+  display.print("Reserved:");
+  display.print(WiFi.localIP());
+  display.display();
 
   // method for sensors
   app.get("/led", &readLed);
